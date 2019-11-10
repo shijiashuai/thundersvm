e0abb530f6db70e66561193a4477b31de4fc23e3
shijiashuai <shijiashuai@gmail.com>
2019-11-10 11:08:42 +0800

    test

--- python/setup.py
+++ python/setup.py
@@ -3,33 +3,51 @@ import setuptools
 from shutil import copyfile
 from sys import platform
 
-
 dirname = path.dirname(path.abspath(__file__))
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+
+    class bdist_wheel(_bdist_wheel):
+
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
+            self.root_is_pure = False
+
+        def get_tag(self):
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            # We don't contain any python source
+            python, abi = 'py2.py3', 'none'
+            return python, abi, plat
+except ImportError:
+    bdist_wheel = None
+
 if platform == "linux" or platform == "linux2":
-	lib_path = path.abspath(path.join(dirname, '../build/lib/libthundersvm.so'))
+    lib_path = path.abspath(path.join(dirname, '../build/lib/libthundersvm.so'))
 elif platform == "win32":
-	lib_path = path.abspath(path.join(dirname, '../build/bin/Debug/thundersvm.dll'))
+    lib_path = path.abspath(path.join(dirname, '../build/bin/Debug/thundersvm.dll'))
 elif platform == "darwin":
-	lib_path = path.abspath(path.join(dirname, '../build/lib/libthundersvm.dylib'))
-else :
-	raise EnvironmentError("OS not supported!")
+    lib_path = path.abspath(path.join(dirname, '../build/lib/libthundersvm.dylib'))
+else:
+    raise EnvironmentError("OS not supported!")
 
 if not path.exists(path.join(dirname, "thundersvm", path.basename(lib_path))):
-	copyfile(lib_path, path.join(dirname, "thundersvm", path.basename(lib_path)))
+    copyfile(lib_path, path.join(dirname, "thundersvm", path.basename(lib_path)))
 setuptools.setup(name="thundersvm",
-			     version="0.3.3",
-				 packages=["thundersvm"],
-				 package_dir={"python": "thundersvm"},
-			     description="A Fast SVM Library on GPUs and CPUs",
-			     long_description="The mission of ThunderSVM is to help users easily and efficiently apply SVMs to solve problems. ThunderSVM exploits GPUs and multi-core CPUs to achieve high efficiency",
-			     long_description_content_type="text/plain",
-			     url="https://github.com/zeyiwen/thundersvm",
-			     package_data = {"thundersvm": [path.basename(lib_path)]},
-			     install_requires=['numpy','scipy','scikit-learn'],
-			     classifiers=[
-			                  "Programming Language :: Python :: 3",
-			                  "License :: OSI Approved :: Apache Software License",
-			    ],
-				 python_requires=">=3"
-)
+                 version="0.3.3",
+                 packages=["thundersvm"],
+                 package_dir={"python": "thundersvm"},
+                 description="A Fast SVM Library on GPUs and CPUs",
+                 long_description="The mission of ThunderSVM is to help users easily and efficiently apply SVMs to solve problems. ThunderSVM exploits GPUs and multi-core CPUs to achieve high efficiency",
+                 long_description_content_type="text/plain",
+                 url="https://github.com/zeyiwen/thundersvm",
+                 package_data={"thundersvm": [path.basename(lib_path)]},
+                 install_requires=['numpy', 'scipy', 'scikit-learn'],
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: Apache Software License",
+                 ],
+                 python_requires=">=3",
+                 )
