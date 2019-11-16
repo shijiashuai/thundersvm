8f91a5b346011f96229f501648c5af2362b66854
Jiashuai Shi <shijiashuai@gmail.com>
2019-11-16 21:12:11 +0800

    test

--- scripts/build_gpu_wheel.sh
+++ scripts/build_gpu_wheel.sh
@@ -13,7 +13,7 @@ linux)
     wget https://developer.download.nvidia.com/compute/cuda/repos/${UBUNTU_VERSION}/x86_64/7fa2af80.pub
     sudo apt-key add 7fa2af80.pub
     sudo apt update -qq
-    sudo apt install -y cuda-core-${CUDA_SHORT/./-} cuda-cudart-dev-${CUDA_SHORT/./-} cuda-cufft-dev-${CUDA_SHORT/./-} cuda-cusparse-dev-${CUDA_SHORT/./-}
+    sudo apt install -y cuda-core-${CUDA_SHORT/./-} cuda-cudart-dev-${CUDA_SHORT/./-} cuda-cusparse-dev-${CUDA_SHORT/./-}
     sudo apt clean
     export CUDA_HOME=/usr/local/cuda-${CUDA_SHORT}
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
@@ -24,10 +24,10 @@ osx)
    ;;
 windows)
     wget -q http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_426.00_win10.exe
-    ./cuda_10.1.243_426.00_win10.exe -s
+    ./cuda_10.1.243_426.00_win10.exe -s nvcc_10.1 cusparse_10.1 cudart_10.1 visual_studio_integration_10.1
     export PATH=${MSBUILD_PATH}:$PATH
-    ls $CUDA_HOME
-    ls $CUDA_ROOT_TOOLKIT_DIR
+    ls "$CUDA_HOME"
+    ls "$CUDA_TOOLKIT_ROOT_DIR"
     cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -G "Visual Studio 14 2015 Win64" ..
     choco install python --version=3.6.3
     python -m pip install --upgrade pip
