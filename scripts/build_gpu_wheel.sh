577a6d3b3bf246f64975492e4750767f00b13a26
Jiashuai Shi <shijiashuai@gmail.com>
2019-11-17 00:51:59 +0800

    test

--- scripts/build_gpu_wheel.sh
+++ scripts/build_gpu_wheel.sh
@@ -24,9 +24,7 @@ osx)
    ;;
 windows)
     wget -q http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_426.00_win10.exe
-#    ./cuda_10.1.243_win10_network.exe -s
-#    7z x -y cuda_10.1.243_426.00_win10.exe
-    ./cuda_10.1.243_426.00_win10.exe -s nvcc_10.1
+    ./cuda_10.1.243_426.00_win10.exe -s nvcc_10.1 cusparse_dev_10.1
     ls "/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1"
     export PATH=${MSBUILD_PATH}:$PATH
     cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -DCUDA_TOOLKIT_ROOT_DIR="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1" -G "Visual Studio 14 2015 Win64" ..
