bf34acaa7a5e5f8d8bdec360b09928dc70afb412
Jiashuai Shi <shijiashuai@gmail.com>
2019-11-16 11:54:11 +0800

    test

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -11,7 +11,6 @@ osx)
 windows)
     mkdir build-test
     cd build-test
-    cmake -DBUILD_TESTS=ON -DUSE_CUDA=OFF -DUSE_EIGEN=ON ..
     export PATH=${MSBUILD_PATH}:$PATH
     cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -DUSE_CUDA=OFF -DUSE_EIGEN=ON -DBUILD_TESTS=ON -G "Visual Studio 14 2015 Win64" ..
     ;;
