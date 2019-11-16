3f369a631113e6f027586b5620916caa68705b52
Jiashuai Shi <shijiashuai@gmail.com>
2019-11-17 00:27:37 +0800

    test

--- scripts/build_gpu_wheel.sh
+++ scripts/build_gpu_wheel.sh
@@ -27,8 +27,9 @@ windows)
 #    ./cuda_10.1.243_win10_network.exe -s
 #    7z x -y cuda_10.1.243_426.00_win10.exe
     ./cuda_10.1.243_426.00_win10.exe -s nvcc_10.1
+    ls "/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1"
     export PATH=${MSBUILD_PATH}:$PATH
-    cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -DCUDA_TOOLKIT_ROOT_DIR="./CUDAToolkit" G "Visual Studio 14 2015 Win64" ..
+    cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -DCUDA_TOOLKIT_ROOT_DIR="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1" -G "Visual Studio 14 2015 Win64" ..
     choco install python --version=3.6.3
     python -m pip install --upgrade pip
     pip install wheel
